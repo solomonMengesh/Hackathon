19f50e167c9e1957efb7196c6a0b3ae6f182b0ee
ahma5050 <tekisha5050@gmail.com>
2025-05-21 11:28:11 -0700

    url

--- backend/server.js
+++ backend/server.js
@@ -4,19 +4,20 @@ import dotenv from 'dotenv';
 import authRoutes from "./routes/authRoutes.js";
 import connectDB from "./config/db.js";
 import routes from "./routes/index.js";
+import mongoose from 'mongoose';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT=process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
-    await connectDB();
+
 
     app.use(cors());
     app.use(express.json());
-
+ const url=process.env.url;
     app.use("/api/auth", authRoutes);
     app.use("/api", routes);
 
@@ -24,9 +25,11 @@ const startServer = async () => {
       res.send('API is running...');
     });
 
-    app.listen(PORT, () => {
-      console.log(`Server is running on http://localhost:${PORT}`);
-    });
+ app.listen(PORT, ()=>{console.log(`the server is runing ${PORT}`)});
+ mongoose.connect(url)
+ .then(()=>console.log("the connection is created successfully !!"))
+ .catch((err) => console.log("Connection failed:", err.message));
+ 
 
   } catch (error) {
     console.error("Failed to start server:", error);
